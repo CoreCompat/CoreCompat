adf34e0f5f3992bdaf4a7f78a9afd0e19ffd67b7
Frederik Carlier <frederik.carlier@quamotion.mobi>
2016-08-30 14:52:29 +0200

    Include dependency .dylibs, and make sure the linker path is set to a relative path

--- native/runtime.osx.10.10-x64.CoreCompat.System.Drawing/build.sh
+++ native/runtime.osx.10.10-x64.CoreCompat.System.Drawing/build.sh
@@ -14,4 +14,35 @@ make clean
 make
 make install
 
+# Copy the dependent dylibs from /usr/local/opt to the $out dir
+dylibs=`otool -L $out/lib/libgdiplus.dylib | grep "/usr/local" | awk -F' ' '{ print $1 }'`
+
+for dylib in $dylibs; do 
+  cp $dylib $out/lib/
+done;
+
+# libcairo has a dependency on libpixman, so include that one, too
+cp /usr/local/opt/pixman/lib/libpixman-1.0.dylib $out/lib
+cp /usr/local/lib/libpcre.1.dylib $out/lib
+
+# Patch the dylib dependencies for all .dylib files in the out directory
+for f in $out/lib/*.dylib; do
+   chmod +w $f
+
+   # Skip the first two lines of the otool output, this is just the name of the dylib itself
+   dylibs=`otool -L $f | tail -n +3 | grep "/usr/local" | awk -F' ' '{ print $1 }'`
+
+   for dylib in $dylibs; do
+     basename=`basename $dylib`
+
+     if [ ! -f "$out/lib/$basename" ]; then
+        echo "error: The dylib file $out/lib/$basename does not exist in the output folder; referenced from $f"
+     fi
+
+     # https://www.mikeash.com/pyblog/friday-qa-2009-11-06-linking-and-install-names.html
+     install_name_tool -change $dylib @loader_path/$basename $f
+   done;
+
+   # otool -L $f
+done
 
