8b0641ee1be21091d705564bd76a6c6cfd918565
Frederik Carlier <frederik.carlier@quamotion.mobi>
2016-09-04 00:17:49 +0200

    Link with libexif

--- native/runtime.linux-x64.CoreCompat.System.Drawing/install-dependencies.sh
+++ native/runtime.linux-x64.CoreCompat.System.Drawing/install-dependencies.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-sudo apt-get install -y libgtk2.0-dev libtiff5-dev libjpeg-dev libgif-dev
+sudo apt-get install -y libgtk2.0-dev libtiff5-dev libjpeg-dev libgif-dev libexif-dev
+
+# We need patchelf 0.9; Ubuntu 16.04 includes 0.8 (sigh)
+wget https://nixos.org/releases/patchelf/patchelf-0.9/patchelf-0.9.tar.gz 
+tar -xvzf patchelf-0.9.tar.gz
+cd patchelf-0.9
+./configure
+make
+cd ..
