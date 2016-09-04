31242c551bac376e02851c763c1a9cc0908709e0
Frederik Carlier <frederik.carlier@quamotion.mobi>
2016-09-04 03:06:57 +0200

    Build custom cairo without X11 bindings, add missing dependencies

--- native/runtime.linux-x64.CoreCompat.System.Drawing/build.sh
+++ native/runtime.linux-x64.CoreCompat.System.Drawing/build.sh
@@ -6,6 +6,13 @@ out=$path/out/usr/local
 # Remove the output folder, if it already exists
 rm -rf $out
 
+cd $path/cairo-1.14.6
+./autogen.sh --enable-xlib=no --enable-xlib-xrender=no --enable-xcb=no --enable-xlib-xcb=no --enable-xcb-shm=no --prefix=$out
+make clean
+make
+make install
+rm -rf $path/cairo-1.14.6/util/cairo-script/examples
+
 cd $path/libgdiplus
 ./autogen.sh --prefix=$out --without-x11
 make clean
@@ -13,7 +20,6 @@ make
 make install
 
 cp /lib/x86_64-linux-gnu/libglib-2.0.so.0 $out/lib
-cp /usr/lib/x86_64-linux-gnu/libcairo.so.2 $out/lib
 cp /usr/lib/x86_64-linux-gnu/libfreetype.so.6 $out/lib
 cp /usr/lib/x86_64-linux-gnu/libjpeg.so.8 $out/lib
 cp /usr/lib/x86_64-linux-gnu/libtiff.so.5 $out/lib
@@ -22,6 +28,7 @@ cp /usr/lib/x86_64-linux-gnu/libpng12.so.0 $out/lib
 cp /usr/lib/x86_64-linux-gnu/libexif.so.12 $out/lib
 cp /usr/lib/x86_64-linux-gnu/libfontconfig.so.1 $out/lib
 cp /usr/lib/x86_64-linux-gnu/libpixman-1.so.0 $out/lib
+cp /usr/lib/x86_64-linux-gnu/libjbig.so.0 $out/lib
 
 # For more info, see: https://github.com/NixOS/patchelf
 # http://man7.org/linux/man-pages/man8/ld.so.8.html
